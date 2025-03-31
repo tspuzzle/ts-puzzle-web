778a28f242117b816607b97ef5deca39024f4c00
tspuzzle <typescriptchallenges@gmail.com>
2025-03-31 21:01:58 +0200

    chore: init appliacation (#3)
    Co-authored-by: vladtopolev <vladimir.topolev@numatic.co>

--- src/migrations/index.ts
+++ src/migrations/index.ts
@@ -1,9 +1,9 @@
-import * as migration_20250331_125547_init_schema from './20250331_125547_init_schema';
+import * as migration_20250331_185942_init_shcema from './20250331_185942_init_shcema';
 
 export const migrations = [
   {
-    up: migration_20250331_125547_init_schema.up,
-    down: migration_20250331_125547_init_schema.down,
-    name: '20250331_125547_init_schema'
+    up: migration_20250331_185942_init_shcema.up,
+    down: migration_20250331_185942_init_shcema.down,
+    name: '20250331_185942_init_shcema'
   },
 ];
