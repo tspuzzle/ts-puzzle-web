0842ef2901651deae4b610e6bd972c5654592a23
vladtopolev <vladimir.topolev@numatic.co>
2025-04-01 20:16:19 +0200

    feat: challenge page

--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -17,12 +17,14 @@ const buttonVariants = cva(
         secondary: 'bg-secondary text-secondary-foreground shadow-xs hover:bg-secondary/80',
         ghost: 'hover:bg-accent hover:text-accent-foreground dark:hover:bg-accent/50',
         link: 'text-primary underline-offset-4 hover:underline',
+        navigation: 'h-6.25 text-primary-text  hover:text-secondary-text',
       },
       size: {
         default: 'h-10.5 px-4 py-2 has-[>svg]:px-3',
         sm: 'h-8.5 rounded-lg gap-1.5 px-3 has-[>svg]:px-2.5',
         lg: 'h-10 rounded-lg px-6 has-[>svg]:px-4',
         icon: 'size-9',
+        navigation: 'h-6.25 px-2 rounded-xs',
       },
     },
     defaultVariants: {
