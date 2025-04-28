7eed02d8d419432641e5ac92efa541fbcff42e01
tspuzzle <typescriptchallenges@gmail.com>
2025-04-28 17:17:01 +0200

    feat: add test content (#41)
    Co-authored-by: vladtopolev <vladimir.topolev@numatic.co>

--- src/migrations/index.ts
+++ src/migrations/index.ts
@@ -1,6 +1,7 @@
 import * as migration_20250331_185942_init_shcema from './20250331_185942_init_shcema';
 import * as migration_20250401_210314_add_challenge_collection from './20250401_210314_add_challenge_collection';
 import * as migration_20250422_100319_content_for_chapter from './20250422_100319_content_for_chapter';
+import * as migration_20250428_151402_add_test_content from './20250428_151402_add_test_content';
 
 export const migrations = [
   {
@@ -16,6 +17,11 @@ export const migrations = [
   {
     up: migration_20250422_100319_content_for_chapter.up,
     down: migration_20250422_100319_content_for_chapter.down,
-    name: '20250422_100319_content_for_chapter'
+    name: '20250422_100319_content_for_chapter',
+  },
+  {
+    up: migration_20250428_151402_add_test_content.up,
+    down: migration_20250428_151402_add_test_content.down,
+    name: '20250428_151402_add_test_content'
   },
 ];
