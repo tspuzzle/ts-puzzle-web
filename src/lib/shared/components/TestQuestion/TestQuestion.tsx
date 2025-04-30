117d8cc48d7cd882a5ea9b74495de1f4cc57198f
tspuzzle <typescriptchallenges@gmail.com>
2025-04-30 15:23:06 +0200

    feat: test view (#43)
    Co-authored-by: vladtopolev <vladimir.topolev@numatic.co>

--- src/lib/shared/components/TestQuestion/TestQuestion.tsx
+++ src/lib/shared/components/TestQuestion/TestQuestion.tsx
@@ -2,13 +2,32 @@ import RichText from '../RichText'
 import { AnswerCard } from './components/AnswerCard'
 import { Question } from './model'
 
-export const TestQuestion = ({ question }: { question: Question }) => {
+export const TestQuestion = ({
+  question,
+  mode,
+  checkedAnswers,
+  setCheckedAnswers,
+}: {
+  question: Question
+  checkedAnswers: number[]
+  mode: 'picking' | 'review'
+  setCheckedAnswers: (checkedAnswers: number[]) => void
+}) => {
   return (
     <div>
       <RichText data={question.question} />
-      <div>
+      <div className="flex flex-col gap-4 mt-8">
         {(question.answers || []).map((answer, index) => (
-          <AnswerCard key={index} answer={answer} index={index} />
+          <AnswerCard
+            key={index}
+            answer={answer}
+            index={index}
+            mode={mode}
+            checked={checkedAnswers.includes(index)}
+            onClick={() => {
+              setCheckedAnswers([index])
+            }}
+          />
         ))}
       </div>
     </div>
